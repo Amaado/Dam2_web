7691a5a9688a97ae2327ab35029e62e795ce5e92
Amado <andresamadocibreiro22@gmail.com>
2024-09-15 14:27:57 +0200

    .

--- main.js
+++ main.js
@@ -4,14 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 	setTimeout(function() {
-		cursorPurpleish.style.visibility = 'hidden';
+		cursorPurpleish.style.opacity = '1%';
 		
 	}, 1);
 
 
-    // Verifica que los elementos existen antes de agregar eventos
     if (cursor && cursorPurpleish) {
-        // Inicialmente fija la posición del cursor
         document.addEventListener('mousemove', function(e) {
             cursor.style.left = e.clientX + 'px';
             cursor.style.top = e.clientY + 'px';
@@ -19,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
             cursorPurpleish.style.top = e.clientY + 'px';
         });
 
-        // Maneja el cambio de estado de clic para ambos cursores
         document.addEventListener('mousedown', function() {
             cursor.classList.add('clicked');
             cursorPurpleish.classList.add('clicked');
@@ -30,19 +27,18 @@ document.addEventListener('DOMContentLoaded', function() {
             cursorPurpleish.classList.remove('clicked');
         });
 
-        // Maneja la visibilidad del cursor cuando se pasa sobre un enlace
 		document.addEventListener('mouseover', function(e) {
 			if (e.target.tagName === 'A' || e.target.closest('.card')  || e.target.closest('.material-icons-round')) {
-				// Verifica si el ratón está sobre un enlace o un elemento con la clase 'card'
-				cursorPurpleish.style.visibility = 'visible'; // Cambia la visibilidad del cursor
+                cursorPurpleish.style.opacity = '100%';
+				//cursorPurpleish.style.visibility = 'visible';
 				//cursor.style.visibility = 'hidden';
 			}
 		});
 		
 		document.addEventListener('mouseout', function(e) {
 			if (e.target.tagName === 'A' || e.target.closest('.card')  || e.target.closest('.material-icons-round')) {
-				// Verifica si el ratón ha salido de un enlace o un elemento con la clase 'card'
-				cursorPurpleish.style.visibility = 'hidden';
+                cursorPurpleish.style.opacity = '1%';
+				//cursorPurpleish.style.visibility = 'hidden';
 				//cursor.style.visibility = 'visible';
 			}
 		});
